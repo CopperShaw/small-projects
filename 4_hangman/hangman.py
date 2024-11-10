d0b3be0ddb9fb49fbc4cda8534e312aa423f8934
Andrei Constantin <learn.cortex@gmail.com>
2024-11-10 10:07:15 +0200

    add 4_hangman

---
+++ 4_hangman/hangman.py
@@ -0,0 +1,9 @@
+'''
+Program outline:
+    1. User is presented with the hidden word.
+    2. User is asked to type a letter.
+    3. Program check if letter is in word and responds accordingly.
+        3.1 Letter in word: Display the hidden word + guessed letters + ask for input if word not completely guessed and tries > 0.
+        3.2 Letter not in word: Display the hidden word + ask for input + decrease tries.
+        3.3 Input not a letter: Ask for input again.
+'''
---
+++ 4_hangman/tests.py
@@ -0,0 +1,6 @@
+'''
+Tests:
+    - Test input is a letter or the correct word +!.
+    - Test correct letter works +!.
+    - Test tries behaviour: good guesses leave tries, bad guesses decrease tries.
+'''
