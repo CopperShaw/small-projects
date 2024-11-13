27d6e3a8c13802fe47fb8a06e258ddc833a7eee5
Andrei Constantin <learn.cortex@gmail.com>
2024-11-13 17:02:09 +0200

    complete 4_hangman

--- 4_hangman/hangman.py
+++ 4_hangman/hangman.py
@@ -7,3 +7,43 @@ Program outline:
         3.2 Letter not in word: Display the hidden word + ask for input + decrease tries.
         3.3 Input not a letter: Ask for input again.
 '''
+import random
+
+def get_input_letter():
+    letter = input('Guess a letter: ')
+
+    if isinstance(letter, str):
+        return letter
+    
+    return None
+
+def main():
+    words = ['banana', 'apple', 'sauce']
+    chosen_word = random.choice(words)
+
+    tries = len(chosen_word)
+    good_guesses = ''
+
+    while tries > 0:
+        print('Word: ', end=' ')
+        for letter in chosen_word:
+            if letter in good_guesses:
+                print(letter, end=' ')
+            else:
+                print('_', end=' ')
+        print()
+        
+        guess = get_input_letter()
+        if guess in chosen_word:
+            if guess not in good_guesses:
+                good_guesses += guess
+            else:
+                print(f'You already guessed {guess}')
+        else:
+            tries -=1
+            print(f'Wrong guess, {tries} tries remaining!')
+
+
+
+if __name__ == '__main__':
+    main()
--- 4_hangman/tests.py
+++ 4_hangman/tests.py
@@ -1,6 +1,22 @@
 '''
 Tests:
-    - Test input is a letter or the correct word +!.
+    - Test input is a letter +!.
     - Test correct letter works +!.
     - Test tries behaviour: good guesses leave tries, bad guesses decrease tries.
 '''
+
+from hangman import get_input_letter
+
+def test_user_input(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _:'a')
+    inp = get_input_letter()
+    # ensures strings work as input
+    assert inp == 'a'
+
+    monkeypatch.setattr('builtins.input', lambda _:27)
+    inp = get_input_letter()
+    # ensures non strings don't work as input
+    assert inp is None
+
+def test_correct_letter(monkeypatch, capsys):
+    monkeypatch.setattr('builtins.input', lambda _:'a')
